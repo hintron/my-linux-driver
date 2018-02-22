36eee49e534300e2706a15bbfede2ceb2a12b23b
Michael Hinton <michael.g.hinton@gmail.com>
2018-02-22 16:41:40 -0700

    Created readme; Capitalized Makefile; massaged the driver code a bit

--- README.md
+++ README.md
@@ -2,5 +2,26 @@
 
 I am going through a linux loadable kernel module (i.e. linux driver) tutorial.
 
+
+### Requirements:
+
+Linux OS (I'm doing this on a Debian 9.3 Digital Ocean droplet)
+
+Packages to install (sudo apt-get install <package>):
+git
+build-essential
+linux-headers-4.9.0-5-amd64
+    (run `apt-cache search linux-headers-$(uname -r)` to figure out the exact package for your linux distro)
+
+
+git clone https://github.com/hintron/my-linux-driver
+
+cd my-linux-driver
+make
+
+
+
+
+
 See http://derekmolloy.ie/writing-a-linux-kernel-module-part-1-introduction/
 See http://derekmolloy.ie/writing-a-linux-kernel-module-part-2-a-character-device/
--- my_linux_driver.c
+++ my_linux_driver.c
@@ -1,25 +1,75 @@
 /**
- * @file    hello.c
+ * @file    my_linux_driver.c
  * @author  Michael Hinton
- * @date    4 April 2018
+ * @date    2 February 2018
  * @version 0.1
- * @brief  An introductory "Hello World!" loadable kernel module (LKM) that can display a message
- * in the /var/log/kern.log file when the module is loaded and removed. The module can accept an
- * argument when it is loaded -- the name, which appears in the kernel log files.
- * @see http://derekmolloy.ie/writing-a-linux-kernel-module-part-1-introduction/ for a full description and follow-up descriptions.
+ * @brief  An introductory loadable kernel module (LKM) for educational purposes
+ * in the /var/log/kern.log file when the module is loaded and removed. The module
+ * can accept an argument when it is loaded -- the name, which appears in the kernel
+ * log files.
+ * @see http://derekmolloy.ie/writing-a-linux-kernel-module-part-1-introduction/
+ * @see http://derekmolloy.ie/writing-a-linux-kernel-module-part-2-a-character-device/
 */
 
-#include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
-#include <linux/module.h>           // Core header for loading LKMs into the kernel
-#include <linux/kernel.h>           // Contains types, macros, functions for the kernel
+// Macros used to mark up functions e.g., __init __exit
+#include <linux/init.h>
+// Core header for loading LKMs into the kernel
+#include <linux/module.h>
+// Contains types, macros, functions for the kernel
+#include <linux/kernel.h>
 
-MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
-MODULE_AUTHOR("Michael Hinton");      ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux driver for educational purposes");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");              ///< The version of the module
 
-static char *name = "world";        ///< An example LKM argument -- default value is "world"
-module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+// // Header to support the kernel Driver Model
+// #include <linux/device.h>
+// // Header for the Linux file system support
+// #include <linux/fs.h>
+// // Required for the copy_to_user function
+// #include <linux/uaccess.h>
+// ///< The device will appear at /dev/ebbchar using this value
+// #define  DEVICE_NAME "my_linux_driver"
+// ///< The device class -- this is a character device driver
+// #define  CLASS_NAME  "mgh"
+
+
+
+
+///< The license type -- this affects runtime behavior
+MODULE_LICENSE("GPL");
+///< The author -- visible when you use modinfo
+MODULE_AUTHOR("Michael Hinton");
+///< The description -- see modinfo
+MODULE_DESCRIPTION("A simple Linux driver for educational purposes");
+///< The version of the module
+MODULE_VERSION("0.1");
+
+
+
+// ///< Stores the device number -- determined automatically
+// static int    majorNumber;
+// ///< Memory for the string that is passed from userspace
+// static char   message[256] = {0};
+// ///< Used to remember the size of the string stored
+// static short  size_of_message;
+// ///< Counts the number of times the device is opened
+// static int    numberOpens = 0;
+// ///< The device-driver class struct pointer
+// static struct class*  ebbcharClass  = NULL;
+// ///< The device-driver device struct pointer
+// static struct device* ebbcharDevice = NULL;
+
+// // The prototype functions for the character driver -- must come before the struct definition
+// static int     dev_open(struct inode *, struct file *);
+// static int     dev_release(struct inode *, struct file *);
+// static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+// static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+
+
+
+///< An example LKM argument -- default value is "world"
+static char *name = "world";
+///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+module_param(name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
 
 /** @brief The LKM initialization function
